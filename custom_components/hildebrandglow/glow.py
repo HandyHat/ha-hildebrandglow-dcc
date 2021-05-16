9f9c2f6b59b119fbc814901b844209fa5dcfd53b
Harley Watson <htw@lobi.to>
2021-05-16 23:51:22 +0100

    connect to MQTT server with discovered CAD ID

--- custom_components/hildebrandglow/glow.py
+++ custom_components/hildebrandglow/glow.py
@@ -5,11 +5,17 @@ from typing import Any, Dict, List
 import requests
 from homeassistant import exceptions
 
+from hbmqtt.client import MQTTClient
+from hbmqtt.mqtt.constants import QOS_1
+
 
 class Glow:
     """Bindings for the Hildebrand Glow Platform API."""
 
     BASE_URL = "https://api.glowmarkt.com/api/v0-1"
+
+    hardwareId: str
+    broker: MQTTClient
 
     def __init__(self, app_id: str, token: str):
         """Create an authenticated Glow object."""
@@ -55,6 +61,46 @@ class Glow:
 
         data = response.json()
         return data
+
+    def retrieve_cad_hardwareId(self) -> str:
+        """Locate the Consumer Access Device's hardware ID from the devices list."""
+        ZIGBEE_GLOW_STICK = "1027b6e8-9bfd-4dcb-8068-c73f6413cfaf"
+
+        devices = self.retrieve_devices()
+
+        cad: Dict[str, Any] = next(
+            (dev for dev in devices if dev["deviceTypeId"] == ZIGBEE_GLOW_STICK), None
+        )
+
+        self.hardwareId = cad["hardwareId"]
+
+        return self.hardwareId
+
+    async def connect_mqtt(self) -> None:
+        """Connect the internal MQTT client to the discovered CAD"""
+        HILDEBRAND_MQTT_HOST = (
+            "mqtts://USER:PASS@glowmqtt.energyhive.com/"
+        )
+        HILDEBRAND_MQTT_TOPIC = "SMART/HILD/{hardwareId}"
+
+        cad_hwId = self.hardwareId
+        topic = HILDEBRAND_MQTT_TOPIC.format(hardwareId=cad_hwId)
+
+        self.broker = MQTTClient()
+
+        await self.broker.connect(HILDEBRAND_MQTT_HOST)
+
+        await self.broker.subscribe(
+            [
+                (topic, QOS_1),
+            ]
+        )
+
+    async def retrieve_mqtt(self) -> None:
+        while True:
+            message = await self.broker.deliver_message()
+            packet = message.publish_packet.payload.data.decode()
+            print(packet)
 
     def retrieve_resources(self) -> List[Dict[str, Any]]:
         """Retrieve the resources known to Glowmarkt for the authenticated user."""
