24bf8d7d4b873c5887a4d9da044df07a35917c6a
Harley Watson <htw@lobi.to>
2021-05-16 22:50:19 +0100

    calculate MQTT topic based on CAD ID

--- custom_components/hildebrandglow/__init__.py
+++ custom_components/hildebrandglow/__init__.py
@@ -40,9 +40,35 @@ async def handle_failed_auth(config: ConfigEntry, hass: HomeAssistant) -> None:
     hass.data[DOMAIN][config.entry_id] = glow
 
 
+async def retrieve_cad_hardwareId(hass: HomeAssistant, glow: Glow) -> str:
+    """Locate the Consumer Access Device's hardware ID from the devices list."""
+    ZIGBEE_GLOW_STICK = "1027b6e8-9bfd-4dcb-8068-c73f6413cfaf"
+
+    devices = await hass.async_add_executor_job(glow.retrieve_devices)
+
+    cad: Dict[str, Any] = next(
+        (dev for dev in devices if dev["deviceTypeId"] == ZIGBEE_GLOW_STICK), None
+    )
+
+    return cad["hardwareId"]
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Hildebrand Glow from a config entry."""
     glow = Glow(APP_ID, entry.data["token"])
+
+    try:
+        hardwareId: str = await retrieve_cad_hardwareId(hass, glow)
+        mqtt_topic: str = f"SMART/HILD/{hardwareId}"
+
+        resources = await hass.async_add_executor_job(glow.retrieve_resources)
+
+    except InvalidAuth:
+        try:
+            await handle_failed_auth(entry, hass)
+            return False
+        except InvalidAuth:
+            return False
+
     hass.data[DOMAIN][entry.entry_id] = glow
 
     for component in PLATFORMS:
