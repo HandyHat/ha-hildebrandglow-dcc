bf81fe5b4333a3e3680f620e2d3b8e1f72bf286f
totalitarian <l.fixter@gmail.com>
2021-09-18 13:12:30 +0100

    added m3 sensor

--- custom_components/hildebrandglow_dcc/sensor.py
+++ custom_components/hildebrandglow_dcc/sensor.py
@@ -5,12 +5,16 @@ from typing import Any, Callable, Dict, Optional
 import pytz
 from homeassistant.components.sensor import (
     DEVICE_CLASS_ENERGY,
+    DEVICE_CLASS_GAS,
     STATE_CLASS_TOTAL_INCREASING,
     DEVICE_CLASS_MONETARY,
     SensorEntity,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ENERGY_KILO_WATT_HOUR
+from homeassistant.const import (
+    ENERGY_KILO_WATT_HOUR,
+    VOLUME_CUBIC_METERS,
+)
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
@@ -44,10 +48,74 @@ async def async_setup_entry(
             if resource["classifier"] in GlowConsumptionCurrent.knownClassifiers:
                 sensor = GlowConsumptionCurrent(glow, resource)
                 new_entities.append(sensor)
+                if resource["classifier"] == "gas.consumption":
+                    sensor = M3Sensor(glow, resource)
+                    new_entities.append(sensor)
 
         async_add_entities(new_entities)
 
     return True
+
+
+class M3Sensor(SensorEntity):
+    """Representation of a Sensor."""
+
+    _attr_state_class = STATE_CLASS_TOTAL_INCREASING
+
+    def __init__(self, glow: Glow, resource: Dict[str, Any]):
+        """Initialize the sensor."""
+        self._state: Optional[Dict[str, Any]] = None
+        self.glow = glow
+        self.resource = resource
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique identifier string for the sensor."""
+        return self.resource["resourceId"] + "m3"
+
+    @property
+    def name(self) -> str:
+        """Return the name of the sensor."""
+        return "Gas Consumption m³ (Today)"
+
+    @property
+    def icon(self) -> Optional[str]:
+        """Icon to use in the frontend, if any."""
+        return "mdi:fire"
+
+    @property
+    def state(self) -> Optional[str]:
+        """Return the state of the sensor."""
+        if self._state:
+            return float(self._state["data"][0][1]) * 3.6 / 40 / 1.02264
+
+    @property
+    def unit_of_measurement(self) -> str:
+        """Return the unit of measurement."""
+        return VOLUME_CUBIC_METERS
+
+    @property
+    def device_info(self) -> Optional[Dict[str, Any]]:
+        return {
+            "name": "Smart Gas Meter",
+        }
+
+    @property
+    def device_class(self) -> str:
+        """Return the device class."""
+        return DEVICE_CLASS_GAS
+
+    async def async_update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        try:
+            self._state = await self.hass.async_add_executor_job(
+                self.glow.current_usage, self.resource["resourceId"]
+            )
+        except InvalidAuth:
+            Glow.handle_failed_auth(ConfigEntry, HomeAssistant)
 
 
 class GlowConsumptionCurrent(SensorEntity):
