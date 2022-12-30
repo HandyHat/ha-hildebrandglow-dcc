c9aec06b07f108725176b21ee89a0f47065499ed
HandyHat <58530748+HandyHat@users.noreply.github.com>
2022-12-30 00:43:59 +0000

    Refactor config flow and init

--- custom_components/hildebrandglow_dcc/__init__.py
+++ custom_components/hildebrandglow_dcc/__init__.py
@@ -1,51 +1,33 @@
-"""The Hildebrand Glow integration."""
-import asyncio
-from typing import Any, Dict
+"""The Hildebrand Glow (DCC) integration."""
+from __future__ import annotations
 
-import voluptuous as vol
+from glowmarkt import BrightClient
+
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-from .const import APP_ID, DOMAIN
-from .glow import Glow
+from .const import DOMAIN
 
-CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
-
-PLATFORMS = ["sensor"]
-
-
-async def async_setup(hass: HomeAssistant, _unused: Dict[str, Any]) -> bool:
-    """Set up the Hildebrand Glow component."""
-    hass.data[DOMAIN] = {}
-
-    return True
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Hildebrand Glow from a config entry."""
-    glow = Glow(APP_ID, entry.data["token"])
-    hass.data[DOMAIN][entry.entry_id] = glow
+    """Set up Hildebrand Glow (DCC) from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    glowmarkt = await hass.async_add_executor_job(
+        BrightClient, entry.data["username"], entry.data["password"]
+    )
+    hass.data[DOMAIN][entry.entry_id] = glowmarkt
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(
-                    entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
-    if unload_ok:
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
--- custom_components/hildebrandglow_dcc/config_flow.py
+++ custom_components/hildebrandglow_dcc/config_flow.py
@@ -1,16 +1,22 @@
-"""Config flow for Hildebrand Glow integration."""
+"""Config flow for Hildebrand Glow (DCC) integration."""
+from __future__ import annotations
+
 import logging
-from typing import Any, Dict
+from typing import Any
 
+from glowmarkt import BrightClient
+import requests
 import voluptuous as vol
-from homeassistant import config_entries, core, data_entry_flow
 
-from .const import APP_ID, DOMAIN
-from .glow import CannotConnect, Glow, InvalidAuth
+from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResult
+
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-DATA_SCHEMA = vol.Schema(
+STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required("username"): str,
         vol.Required("password"): str,
@@ -18,56 +24,58 @@ DATA_SCHEMA = vol.Schema(
 )
 
 
-def config_object(data: dict, glow: Dict[str, Any]) -> Dict[str, Any]:
-    """Prepare a ConfigEntity with authentication data and a temporary token."""
-    return {
-        "name": glow["name"],
-        "username": data["username"],
-        "password": data["password"],
-        "token": glow["token"],
-        "token_exp": glow["exp"],
-    }
-
-
-async def validate_input(hass: core.HomeAssistant, data: dict) -> Dict[str, Any]:
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
     """Validate the user input allows us to connect.
 
-    Data has the keys from DATA_SCHEMA with values provided by the user.
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
     """
-    glow = await hass.async_add_executor_job(
-        Glow.authenticate, APP_ID, data["username"], data["password"]
+
+    glowmarkt = await hass.async_add_executor_job(
+        BrightClient, data["username"], data["password"]
     )
+    _LOGGER.debug("Successful Post to %sauth", glowmarkt.url)
 
-    # Return some info we want to store in the config entry.
-    return config_object(data, glow)
+    # Return info that you want to store in the config entry.
+    return {"title": ""}
 
 
-class DomainConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Hildebrand Glow."""
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Hildebrand Glow (DCC)."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.SOURCE_USER
 
     async def async_step_user(
-        self, user_input: Dict = None
-    ) -> data_entry_flow.FlowResult:
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
         """Handle the initial step."""
-        errors = {}
-        if user_input is not None:
-            try:
-                if self.hass is None:
-                    raise AssertionError
-                info = await validate_input(self.hass, user_input)
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+            )
 
-                return self.async_create_entry(title=info["name"], data=info)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidAuth:
+        errors = {}
+
+        try:
+            info = await validate_input(self.hass, user_input)
+        except requests.Timeout as ex:
+            _LOGGER.debug("Timeout: %s", ex)
+            errors["base"] = "timeout_connect"
+        except requests.exceptions.ConnectionError as ex:
+            _LOGGER.debug("Cannot connect: %s", ex)
+            errors["base"] = "cannot_connect"
+        except Exception as ex:  # pylint: disable=broad-except
+            if "Authentication failed" in str(ex):
+                _LOGGER.debug("Authentication Failed")
                 errors["base"] = "invalid_auth"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
+            elif "Expected an authentication token" in str(ex):
+                _LOGGER.debug("Expected an authentication token but didn't get one")
+                errors["base"] = "invalid_auth"
+            else:
+                _LOGGER.exception("Unexpected exception: %s", ex)
                 errors["base"] = "unknown"
+        else:
+            return self.async_create_entry(title=info["title"], data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
