779f4e4fcb3a835c58a225401e36b91bb59d62e8
HandyHat <58530748+HandyHat@users.noreply.github.com>
2022-12-30 01:17:06 +0000

    Handle offline setup failure

--- custom_components/hildebrandglow_dcc/__init__.py
+++ custom_components/hildebrandglow_dcc/__init__.py
@@ -1,13 +1,18 @@
 """The Hildebrand Glow (DCC) integration."""
 from __future__ import annotations
 
+import logging
+
 from glowmarkt import BrightClient
+import requests
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-
+from homeassistant.exceptions import ConfigEntryNotReady
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
@@ -15,9 +20,19 @@ PLATFORMS: list[Platform] = [Platform.SENSOR]
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Hildebrand Glow (DCC) from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    glowmarkt = await hass.async_add_executor_job(
-        BrightClient, entry.data["username"], entry.data["password"]
-    )
+    try:
+        glowmarkt = await hass.async_add_executor_job(
+            BrightClient, entry.data["username"], entry.data["password"]
+        )
+    except requests.Timeout as ex:
+        raise ConfigEntryNotReady(f"Timeout: {ex}") from ex
+    except requests.exceptions.ConnectionError as ex:
+        raise ConfigEntryNotReady(f"Cannot connect: {ex}") from ex
+    except Exception as ex:  # pylint: disable=broad-except
+        raise ConfigEntryNotReady(f"Unexpected exception: {ex}") from ex
+    else:
+        _LOGGER.debug("Successful Post to %sauth", glowmarkt.url)
+
     hass.data[DOMAIN][entry.entry_id] = glowmarkt
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
